60d2d7217d879e15eefdfc96cbddced48a70f3c7
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2021-10-19 20:56:57 +0200

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,68 +1,103 @@
-def on_forever():
-    pass
-forever(on_forever)
-
 def on_logo_touched():
     if input.logo_is_pressed():
-        basic.show_number(randint(1,  6))
-        basic.show_number(6)
+        basic.show_number(randint(1, 6))
     else:
         basic.show_number(randint(0, 10))
-        basic.show_number(10)
 def on_gesture_shake():
     input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-def tlacitkoA():
-    def on_button_pressed_a():
-        pass
-    input.on_button_pressed(Button.A, on_button_pressed_a) 
+def on_button_pressed_a():
+    input.on_button_pressed(Button.A, on_button_pressed_a)
 
-if input.button_is_pressed(Button.A):
-        x = randint(0, 10)
-        if x == 1:
-            basic.show_leds("""
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-        elif x == 2:
-            basic.show_leds("""
-            # # . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-        elif x == 3:
-            basic.show_leds("""
-            # # # . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-        elif x == 4:
-            basic.show_leds("""
-            # # # # .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-        elif x == 5:
-            basic.show_leds("""
-            # # # # #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
-        elif x == 6:
-            basic.show_leds("""
-            # # # # #
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            """)
+    if input.button_is_pressed(Button.A):
+             x = randint(0, 10)
+
+    elif x == 1:
+                    soundExpression.giggle(1)
+                    basic.show_leds("""
+                    # . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 2:
+                    soundExpression.giggle(2)
+                    basic.show_leds("""
+                    # # . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 3:
+                    soundExpression.giggle(3)
+                    basic.show_leds("""
+                    # # # . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 4:
+                    soundExpression.giggle(4)
+                    basic.show_leds("""
+                    # # # # .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 5:
+                    soundExpression.giggle(5)
+                    basic.show_leds("""
+                    # # # # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 6:
+                    soundExpression.giggle(6)
+                    basic.show_leds("""
+                    # # # # #
+                    # . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 7:
+                    soundExpression.giggle(7)
+                    basic.show_leds("""
+                    # # # # #
+                    # # . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)    
+    elif x == 8:
+                    soundExpression.giggle(8)
+                    basic.show_leds("""
+                    # # # # #
+                    # # # . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 9:
+                    soundExpression.giggle(9)
+                    basic.show_leds("""
+                    # # # # #
+                    # # # # .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
+    elif x == 10:
+                    soundExpression.giggle(10)
+                    basic.show_leds("""
+                    # # # # #
+                    # # # # #
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    """)
